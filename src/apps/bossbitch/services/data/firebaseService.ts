6f930c001829a4f0ed45b78285a7820a84099a46
jamesdanielwhitford <james.whitford0@gmail.com>
2025-02-25 11:57:50 +0200

    Fix env variables name

--- src/apps/bossbitch/services/data/firebaseService.ts
+++ src/apps/bossbitch/services/data/firebaseService.ts
@@ -44,12 +44,12 @@ const DEFAULT_PREFERENCES: UserPreferences = {
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY_BOSSBITCH,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN_BOSSBITCH,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_BOSSBITCH,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_BOSSBITCH,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID_BOSSBITCH,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID_BOSSBITCH
 };
 
 /**
